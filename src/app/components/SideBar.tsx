861e1b32c53495a38e0bd1eaac1d7dab83345a98
kimddddh98 <amawang11@gmail.com>
2023-11-21 00:15:26 +0900

    총 할일, 오늘할일 구분

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,5 +1,6 @@
 import NavBar from './NavBar'
 import { baseUrl } from '../core'
+import dayjs from 'dayjs'
 // import { http } from '../core'
 // import { useEffect, useState } from 'react'
 async function getList() {
@@ -9,6 +10,7 @@ async function getList() {
 }
 export default async function SideBar({userInfo}:{userInfo:User}){
   const todos = await getList()
+  const today = todos.filter(todo=>todo.startdate === dayjs(new Date).format('YYYY-MM-DD'))
   // const [todoLength,setLength] = useState(0)
   // useEffect(()=>{
   //   (async ()=>{
@@ -21,7 +23,8 @@ export default async function SideBar({userInfo}:{userInfo:User}){
   return <>
     <nav className='side_bar'>
       <h2 className="side_header">My - Todo</h2>
-      <h3>오늘의 할일이 {todos.length} 건 있습니다.</h3>
+      <h3>할일이 {todos.length} 건 있습니다.</h3>
+      <h3>오늘의 할일이 {today.length} 건 있습니다.</h3>
       <NavBar/>
     </nav>
   </>
