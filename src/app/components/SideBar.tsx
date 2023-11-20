7fa07a2eccce1404acb29dcf4216ea4c24c0b33b
kimddddh98 <amawang11@gmail.com>
2023-11-21 00:12:10 +0900

    글 작성과 동시에 할일 카운트 증가를 위해 서버컴포넌트로 변경

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,21 +1,27 @@
-'use client'
 import NavBar from './NavBar'
-import { http } from '../core'
-import { useEffect, useState } from 'react'
-export default function SideBar({userInfo}:{userInfo:User}){
-  const [todoLength,setLength] = useState(0)
-  useEffect(()=>{
-    (async ()=>{
-      const res = await http('/api/todolist')
-      const data:TodoList[] = await res.data
-      setLength(data.length)
-    })();
-  },[])
+import { baseUrl } from '../core'
+// import { http } from '../core'
+// import { useEffect, useState } from 'react'
+async function getList() {
+  const res = await fetch(baseUrl+'/api/todolist',{next:{revalidate:0}})
+  const data:TodoList[] = await res.json()
+  return data
+}
+export default async function SideBar({userInfo}:{userInfo:User}){
+  const todos = await getList()
+  // const [todoLength,setLength] = useState(0)
+  // useEffect(()=>{
+  //   (async ()=>{
+  //     const res = await http('/api/todolist')
+  //     const data:TodoList[] = await res.data
+  //     setLength(data.length)
+  //   })();
+  // },[])
 
   return <>
     <nav className='side_bar'>
       <h2 className="side_header">My - Todo</h2>
-      <h3>오늘의 할일이 {todoLength} 건 있습니다.</h3>
+      <h3>오늘의 할일이 {todos.length} 건 있습니다.</h3>
       <NavBar/>
     </nav>
   </>
