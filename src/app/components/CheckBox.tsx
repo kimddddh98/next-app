d4d6b583076b15dfd089f94a2061e6e342989c56
kimddddh98 <amawang11@gmail.com>
2023-11-22 00:25:23 +0900

    isdone 요청시 로딩

--- src/app/components/CheckBox.tsx
+++ src/app/components/CheckBox.tsx
@@ -2,13 +2,14 @@
 import { useState } from 'react'
 import { http } from '../core'
 import { useRouter } from 'next/navigation'
-
+import Loading from '../todolist/loading'
 
 export default function CheckBox({todoId,todoIsDone}:{todoId:number,todoIsDone:boolean}){
   const router = useRouter()  
   const [isDone,setIsDone]= useState(todoIsDone)
-
+  const [isLoading,setLoading] = useState(false)
   const changeIsDone = async () => {
+    setLoading(true)
     const res = await http.post('/api/todolist/isdone',
     {
       todoid:todoId,
@@ -16,18 +17,23 @@ export default function CheckBox({todoId,todoIsDone}:{todoId:number,todoIsDone:b
     })
 
     const data = await res.data
+    setLoading(false)
     setIsDone(!isDone)
     router.refresh()
     console.log(data)
   }
   return(
-  
-    <label htmlFor={todoId.toString()} 
+    <>
+    {isLoading&&<Loading/>}
+    {!isLoading&&<label htmlFor={todoId.toString()} 
       className={isDone?'check label':'label'}>
         <input type="checkbox" id={todoId.toString()}  checked={isDone}
       onChange={()=>changeIsDone()}
       />
       <span></span>
-    </label>
+    </label>}
+    </>
+    
+   
   )
 }
