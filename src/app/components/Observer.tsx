5524c335756102cd1752a28713ac2d29f7a3071e
kimddddh98 <amawang11@gmail.com>
2023-11-24 00:08:14 +0900

    reactStrictMode 해제 옵저버 카운트 테스트

--- src/app/components/Observer.tsx
+++ src/app/components/Observer.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useEffect, useRef, useState } from 'react';
+import { http } from '../core';
 
 export default function Observer(){
   const el = useRef<HTMLLIElement|null>(null)
@@ -7,21 +8,29 @@ export default function Observer(){
 
 
 useEffect(() => {
+  const options = {
+    root: null, //기본 null, 관찰대상의 부모요소를 지정
+    rootMargin: "20px", // 관찰하는 뷰포트의 마진 지정
+    threshold: 1.0, // 관찰요소와 얼만큼 겹쳤을 때 콜백을 수행하도록 지정하는 요소
+};
     let observer: IntersectionObserver;
     if (el.current) {
-      observer = new IntersectionObserver((entries, _observer) => {
+      observer = new IntersectionObserver(async (entries, _observer) => {
         if (entries[0].isIntersecting) {
             console.log('IN!');
-            setPage(page +1)
+            setPage((p)=>p+1)
         }
-        }, { threshold: 0.5 });
+        }, options);
         observer.observe(el.current);
     }
     // return () => {
     //     observer.disconnect();
     // };
-}, [el]);
+}, []);
 // useEffect(()=>{console.log(page)},[page])
 
-  return <li ref={el}>{page}</li>
+  return (<>
+  <li ref={el} className='observer'></li>
+  {page}
+  </>)
 }
